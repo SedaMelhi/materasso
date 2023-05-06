2f6dd2f109bcecc86157a6f24daa74cfeab5f060
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-05-07 00:27:40 +0300

    Доверстала страницу Product

--- src/Page/Product/Product.jsx
+++ src/Page/Product/Product.jsx
@@ -1,104 +1,64 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import Slider from './Slider/Slider';
 import Path from '../../components/Path/Path';
 import style from './Product.module.sass';
-//import 'swiper/swiper-bundle.css';
 import 'swiper/css';
-SwiperCore.use([Navigation, Pagination]);
+import BasketSvg from '../../assets/svg/BasketSvg';
+
 const Product = () => {
   const id = useSelector((state) => state.product.id);
-  const swiperRef = useRef(null);
-  const [product, setProduct] = useState([{ images: [] }]);
+  const [product, setProduct] = useState({ images: [] });
   useEffect(() => {
     fetch(`https://storefurniture.pythonanywhere.com/api/product/?id=${id}`)
       .then((res) => res.json())
       .then((data) => {
-        setProduct(data.results);
+        setProduct(data.results[0]);
       });
   }, [id]);
+  console.log(product);
   return (
     <div className="wrap product">
       <div className={style.product}>
         <Path />
-        <div className={style.right}>
-          <Swiper
-            spaceBetween={0}
-            ref={swiperRef}
-            slidesPerView={1}
-            modules={[Navigation, Pagination]}
-            pagination={{
-              clickable: true,
-              bulletClass: style.bullet,
-              bulletActiveClass: style.active,
-              wrapper: style.wrapper,
-              renderBullet: function (index) {
-                if (product[0].images[index]) {
-                  return `
-                  <div class="${style.bullet} ${
-                    swiperRef.current?.activeIndex === index ? style.active : ''
-                  }"  style='background-image: url(${product[0].images[index].image})'>
-                  </div>
-                `;
-                } else {
-                  return '';
-                }
-              },
-              onClick: function (swiper, event) {
-                swiper.slideTo(event.target.getAttribute('data-swiper-slide-index'));
-              },
-            }}
-            navigation={{ prevEl: '.arrow-prev', nextEl: '.arrow-next' }}>
-            {product[0].images.map(({ id, image }) => (
-              <SwiperSlide key={id}>
-                <div className={style.slide} style={{ backgroundImage: `url(${image})` }}></div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          {product[0].images.length > 1 ? (
-            <div className={'arrows ' + style.arrows}>
-              <div className="arrow-prev">
-                <svg
-                  width="60"
-                  height="59"
-                  viewBox="0 0 60 59"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="29.7999" cy="29.4258" r="29.4258" fill="#1F2E36" fillOpacity="0.5" />
-                  <path
-                    d="M33.0686 39.052L23.2594 29.3041L33.0686 19.5562"
-                    stroke="white"
-                    strokeWidth="2.4523"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
+        <div className={style.wrapper}>
+          <div className={style.right}>
+            <Slider product={product} />
+            <h3 className={style.title}>Описание</h3>
+            <p className={style.description}>{product.description}</p>
+            <div className={style.details}>
+              <div className={style.detail}>
+                <div className={style.detail__title}>О товаре</div>
+                <div className={style.detail__row}>
+                  <div className={style.detail__subtitle}>Артикул</div>
+                  <div className={style.detail__text}>{product.sku}</div>
+                </div>
               </div>
-              <div className="arrow-next">
-                <svg
-                  width="60"
-                  height="59"
-                  viewBox="0 0 60 59"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="30.1207" cy="29.4258" r="29.4258" fill="#1F2E36" fillOpacity="0.5" />
-                  <path
-                    d="M26.8497 39.052L36.6589 29.3041L26.8497 19.5562"
-                    stroke="white"
-                    strokeWidth="2.4523"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
+              <div className={style.detail}>
+                <div className={style.detail__title}>Детали</div>
+                <div className={style.detail__row}>
+                  <div className={style.detail__subtitle}>Состав-Каркас</div>
+                  <div className={style.detail__text}>{product.frame_composition}</div>
+                </div>
               </div>
             </div>
-          ) : (
-            ''
-          )}
-          <h3 className={style.title}>Описание</h3>
+          </div>
+          <div className={style.left}>
+            <h2 className={style.headline}>{product.name}</h2>
+            <div className={style.priceText}>{product.price}</div>
+            <div className={style.count}>На складе {product.quantity} шт.</div>
+            <div className={style.btn + ' ' + style.btn_blue}>
+              <span className={style.icon}>
+                <BasketSvg />
+              </span>
+              {(+product.price).toLocaleString('en-US').replace(',', ' ')}
+              <span className={style.sine}>₽</span>
+            </div>
+            <div className={style.btn}>
+              В Рассрочку {(+product.price / 10).toLocaleString('en-US').replace(',', ' ')} ₽/мес
+            </div>
+          </div>
         </div>
-        <div className={style.left}></div>
       </div>
     </div>
   );
---
+++ src/Page/Product/Slider/Slider.jsx
@@ -0,0 +1,103 @@
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRef } from 'react';
+import style from './../Product.module.sass';
+SwiperCore.use([Navigation, Pagination]);
+const Slider = ({ product }) => {
+  const swiperRef = useRef(null);
+
+  const handlePrevClick = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+  const handleNextClick = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+  return (
+    <div className={style.slider__wrap}>
+      <div className={style.imageCount}>{product.images.length} фото</div>
+      <Swiper
+        spaceBetween={0}
+        ref={swiperRef}
+        slidesPerView={1}
+        modules={[Navigation, Pagination]}
+        navigation={{
+          prevEl: '.arrow-prev',
+          nextEl: '.arrow-next',
+          disabledClass: 'arrow-disabled',
+        }}
+        pagination={{
+          clickable: true,
+          bulletClass: style.bullet,
+          bulletActiveClass: style.active,
+          wrapper: style.wrapper,
+          renderBullet: function (index) {
+            if (product.images[index]) {
+              return `
+                  <div class="${style.bullet} ${
+                swiperRef.current?.activeIndex === index ? style.active : ''
+              }"  style='background-image: url(${product.images[index].image})'>
+                  </div>
+                `;
+            } else {
+              return '';
+            }
+          },
+          onClick: function (swiper, event) {
+            swiper.slideTo(event.target.getAttribute('data-swiper-slide-index'));
+          },
+        }}>
+        {product.images.map(({ id, image }) => (
+          <SwiperSlide key={id}>
+            <div className={style.slide} style={{ backgroundImage: `url(${image})` }}></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {product.images.length > 1 ? (
+        <div className={style.arrows}>
+          <div className={'arrow-prev'} onClick={handlePrevClick}>
+            <svg
+              width="60"
+              height="59"
+              viewBox="0 0 60 59"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <circle cx="29.7999" cy="29.4258" r="29.4258" fill="#1F2E36" fillOpacity="0.5" />
+              <path
+                d="M33.0686 39.052L23.2594 29.3041L33.0686 19.5562"
+                stroke="white"
+                strokeWidth="2.4523"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div className={'arrow-next'} onClick={handleNextClick}>
+            <svg
+              width="60"
+              height="59"
+              viewBox="0 0 60 59"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <circle cx="30.1207" cy="29.4258" r="29.4258" fill="#1F2E36" fillOpacity="0.5" />
+              <path
+                d="M26.8497 39.052L36.6589 29.3041L26.8497 19.5562"
+                stroke="white"
+                strokeWidth="2.4523"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
+
+export default Slider;
