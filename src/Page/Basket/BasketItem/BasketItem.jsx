1cb1bb5e213e9966f800031a7990d00003d11fd1
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-05-18 00:15:10 +0300

    закончила мобильный адаптив

--- src/Page/Basket/BasketItem/BasketItem.jsx
+++ src/Page/Basket/BasketItem/BasketItem.jsx
@@ -54,18 +54,42 @@ const BasketItem = ({ name, id, count, image, price, sale, installment }) => {
   return (
     <div className={style.product}>
       <div className={style.product__left}>
-        <div className={style.image}>
-          <img src={image} alt="" />
+        <div className={style.image__wrap}>
+          <div className={style.image}>
+            <img src={image} alt="" />
+          </div>
+          <div className={style.count__wrap + ' ' + style.count__mobile}>
+            <button onClick={decrement} className={style.count__action}>
+              <MinusSvg />
+            </button>
+            <input
+              type="number"
+              className={style.count}
+              min="1"
+              max="100"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button onClick={increment} className={style.count__action}>
+              <PlusSvg />
+            </button>
+          </div>
         </div>
         <div className={style.text}>
           <div className={style.name}>{name}</div>
           <div className={style.size}>
             {installment ? 'товар с рассрочкой' : 'товар без рассрочки'}
           </div>
+          <div className={style.price__all + ' ' + style.price__mobile}>
+            <span className={style.text__all}>{((+price * (100 - sale)) / 100) * value}</span> ₽
+          </div>
+        </div>
+        <div className={style.close + ' ' + style.close__mobile} onClick={() => deleteElemnet(id)}>
+          <CloseSvg />
         </div>
       </div>
       <div className={style.product__right}>
-        <div className={style.count__price}>
+        <div className={style.count__price + ' ' + style.count__pc}>
           <div className={style.count__wrap}>
             <button onClick={decrement} className={style.count__action}>
               <MinusSvg />
@@ -83,14 +107,14 @@ const BasketItem = ({ name, id, count, image, price, sale, installment }) => {
             </button>
           </div>
           <div className={style.price}>
-            <span className={style.text + ' ' + (sale > 0 ? style.lineThrough : '')}>{price}</span>{' '}
-            ₽/ШТ. {sale > 0 ? `(-${sale}%)` : ''}
+            <span className={sale > 0 ? style.lineThrough : ''}>{price}</span> ₽/ШТ.
+            {sale > 0 ? `(-${sale}%)` : ''}
           </div>
         </div>
-        <div className={style.price__all}>
+        <div className={style.price__all + ' ' + style.price__pc}>
           <span className={style.text__all}>{((+price * (100 - sale)) / 100) * value}</span> ₽
         </div>
-        <div className={style.close} onClick={() => deleteElemnet(id)}>
+        <div className={style.close + ' ' + style.close__pc} onClick={() => deleteElemnet(id)}>
           <CloseSvg />
         </div>
       </div>
