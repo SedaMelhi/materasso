93ae2331ee649fa2e65fca508106cbfd3d9f04bd
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-05-18 23:12:06 +0300

    spinner для страницы Product

--- src/Page/Product/Product.jsx
+++ src/Page/Product/Product.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { setBasket } from './../../redux/basketSlice/basketSlice';
 import { useDispatch } from 'react-redux';
 import { setProductId } from '../../redux/productSlice/productSlice';
+import ContentLoader from 'react-content-loader';
 import Slider from './Slider/Slider';
 import qs from 'qs';
 import BasketSvg from '../../assets/svg/BasketSvg';
@@ -29,6 +30,7 @@ const Product = () => {
         .then((res) => res.json())
         .then((data) => {
           setProduct(data.results[0]);
+          setLoad(true);
         });
     } else {
       getData();
@@ -83,92 +85,111 @@ const Product = () => {
   };
   return (
     <div className="wrap product">
-      <div className={style.product}>
-        <Path
-          path={
-            filters.name
-              ? [
-                  { text: 'Главная', link: '/' },
-                  { text: filters.name, link: `/catalog/?page=1&category=${filters.categoryId}` },
-                  { text: product.name, link: '' },
-                ]
-              : [
-                  { text: 'Главная', link: '/' },
+      {load ? (
+        <div className={style.product}>
+          <Path
+            path={
+              filters.name
+                ? [
+                    { text: 'Главная', link: '/' },
+                    { text: filters.name, link: `/catalog/?page=1&category=${filters.categoryId}` },
+                    { text: product.name, link: '' },
+                  ]
+                : [
+                    { text: 'Главная', link: '/' },
 
-                  { text: product.name, link: '' },
-                ]
-          }
-        />
-        <div className={style.wrapper}>
-          <div className={style.right}>
-            <Slider product={product} />
-            <h3 className={style.title}>Описание</h3>
-            <p className={style.description}>{product.description}</p>
-            <div className={style.details}>
-              <div className={style.detail}>
-                <div className={style.detail__title}>О товаре</div>
-                <div className={style.detail__row}>
-                  <div className={style.detail__subtitle}>Артикул</div>
-                  <div className={style.detail__text}>{product.sku}</div>
+                    { text: product.name, link: '' },
+                  ]
+            }
+          />
+          <div className={style.wrapper}>
+            <div className={style.right}>
+              <Slider product={product} />
+              <h3 className={style.title}>Описание</h3>
+              <p className={style.description}>{product.description}</p>
+              <div className={style.details}>
+                <div className={style.detail}>
+                  <div className={style.detail__title}>О товаре</div>
+                  <div className={style.detail__row}>
+                    <div className={style.detail__subtitle}>Артикул</div>
+                    <div className={style.detail__text}>{product.sku}</div>
+                  </div>
                 </div>
-              </div>
-              <div className={style.detail}>
-                <div className={style.detail__title}>Детали</div>
-                <div className={style.detail__row}>
-                  <div className={style.detail__subtitle}>Состав-Каркас</div>
-                  <div className={style.detail__text}>{product.frame_composition}</div>
+                <div className={style.detail}>
+                  <div className={style.detail__title}>Детали</div>
+                  <div className={style.detail__row}>
+                    <div className={style.detail__subtitle}>Состав-Каркас</div>
+                    <div className={style.detail__text}>{product.frame_composition}</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className={style.left}>
-            <h2 className={style.headline}>{product.name}</h2>
-            <div className={style.priceText}>{product.price}</div>
-            <div className={style.count}>На складе {product.quantity} шт.</div>
-            <div
-              className={style.btn + ' ' + style.btn_blue}
-              onClick={(event) => {
-                if (btnClick) {
-                  event.preventDefault();
-                } else {
-                  setBtnClick(true);
-                  addProduct(false);
-                }
-              }}>
-              {btnClick ? (
-                'Товар добавлен'
-              ) : (
-                <>
-                  <span className={style.icon}>
-                    <BasketSvg />
-                  </span>
-                  {(+product.price).toLocaleString('en-US').replace(',', ' ')}
-                  <span className={style.sine}>₽</span>
-                </>
-              )}
-            </div>
-            <div
-              className={style.btn}
-              onClick={(event) => {
-                if (installment) {
-                  event.preventDefault();
-                } else {
-                  setInstallment(true);
-                  addProduct(true);
-                }
-              }}>
-              {installment ? (
-                'Товар добавлен'
-              ) : (
-                <>
-                  В Рассрочку {(+product.price / 10).toLocaleString('en-US').replace(',', ' ')}{' '}
-                  ₽/мес
-                </>
-              )}
+            <div className={style.left}>
+              <h2 className={style.headline}>{product.name}</h2>
+              <div className={style.priceText}>{product.price}</div>
+              <div className={style.count}>На складе {product.quantity} шт.</div>
+              <div
+                className={style.btn + ' ' + style.btn_blue}
+                onClick={(event) => {
+                  if (btnClick) {
+                    event.preventDefault();
+                  } else {
+                    setBtnClick(true);
+                    addProduct(false);
+                  }
+                }}>
+                {btnClick ? (
+                  'Товар добавлен'
+                ) : (
+                  <>
+                    <span className={style.icon}>
+                      <BasketSvg />
+                    </span>
+                    {(+product.price).toLocaleString('en-US').replace(',', ' ')}
+                    <span className={style.sine}>₽</span>
+                  </>
+                )}
+              </div>
+              <div
+                className={style.btn}
+                onClick={(event) => {
+                  if (installment) {
+                    event.preventDefault();
+                  } else {
+                    setInstallment(true);
+                    addProduct(true);
+                  }
+                }}>
+                {installment ? (
+                  'Товар добавлен'
+                ) : (
+                  <>
+                    В Рассрочку {(+product.price / 10).toLocaleString('en-US').replace(',', ' ')}{' '}
+                    ₽/мес
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="load">
+          <div class="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
